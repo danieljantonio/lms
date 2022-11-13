56dab0a6dea8e009076d17b537bf95ed65c4e6a4
Daniel J. Antonio <djedidiaha@gmail.com>
2022-11-14 00:02:55 +0700

    fetch number of classes from db

--- src/pages/teacher/classes.tsx
+++ src/pages/teacher/classes.tsx
@@ -1,12 +1,18 @@
 import { type NextPage } from 'next';
 import TeacherLayout from '../../components/layouts/teacher.layout';
+import { trpc } from '../../lib/trpc';
 
 const ClassesPanel: NextPage = () => {
+	const { data, isLoading } = trpc.class.numberOfClasses.useQuery();
+
+	if (isLoading) return <div>Loading...</div>;
+
 	return (
 		<TeacherLayout>
 			<div className="flex flex-col items-center justify-center gap-2">
 				<div>
 					<h1>This page will list all the classes</h1>
+					<p>You have {data} classes</p>
 				</div>
 			</div>
 		</TeacherLayout>
