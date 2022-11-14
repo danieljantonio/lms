3cf6c9ebf760ba2b89a21a3049ea2cf7d6737362
Daniel J. Antonio <djedidiaha@gmail.com>
2022-11-14 16:51:45 +0700

    remove unused imports and code from landing page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import { type NextPage } from 'next';
 import Head from 'next/head';
-import { signIn, signOut, useSession } from 'next-auth/react';
-import { trpc } from '../lib/trpc';
+import { signIn, signOut } from 'next-auth/react';
 import Link from 'next/link';
 import useAuth from '../lib/hooks/useAuth';
 import { Button } from 'flowbite-react';
@@ -41,21 +40,3 @@ const Home: NextPage = () => {
 };
 
 export default Home;
-
-const AuthShowcase: React.FC = () => {
-	const { data: secretMessage } = trpc.auth.getSecretMessage.useQuery();
-
-	const { data: sessionData } = useSession();
-
-	return (
-		<div className="flex flex-col items-center justify-center gap-2">
-			{sessionData && <p className="text-2xl text-blue-500">Logged in as {sessionData?.user?.name}</p>}
-			{secretMessage && <p className="text-2xl text-blue-500">{secretMessage}</p>}
-			<button
-				className="rounded-md border border-black bg-violet-50 px-4 py-2 text-xl shadow-lg hover:bg-violet-100"
-				onClick={sessionData ? () => signOut() : () => signIn()}>
-				{sessionData ? 'Sign out' : 'Sign in'}
-			</button>
-		</div>
-	);
-};
