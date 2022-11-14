1c7edd3122e18f95478096371890eec2684b1e30
Daniel J. Antonio <djedidiaha@gmail.com>
2022-11-14 12:49:11 +0700

    add join school function

--- src/server/trpc/router/school.ts
+++ src/server/trpc/router/school.ts
@@ -1,3 +1,4 @@
+import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
 import { protectedProcedure, router } from '../trpc';
 
@@ -15,5 +16,30 @@ export const schoolRouter = router({
 			});
 
 			return school;
+		}),
+	join: protectedProcedure
+		.input(
+			z.object({
+				invite: z.string(),
+			}),
+		)
+		.mutation(async ({ input, ctx }) => {
+			const { id, schoolId } = ctx.session.user;
+			if (schoolId)
+				throw new TRPCError({
+					code: 'PRECONDITION_FAILED',
+					message: 'The user is already in a school',
+				});
+			const school = await ctx.prisma.school.findUnique({ where: { invite: input.invite } });
+			if (!school)
+				throw new TRPCError({
+					code: 'NOT_FOUND',
+					message: `School with invite code ${input.invite} not found.`,
+				});
+			const updateUser = await ctx.prisma.user.update({
+				where: { id },
+				data: { schoolId: school.id },
+			});
+			return updateUser;
 		}),
 });
--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -12,6 +12,8 @@ declare module 'next-auth' {
 			name: string;
 			email: string;
 			role: Roles;
+			school: string; //TODO: change this to the appropriate type once it is set up
+			schoolId: string; //TODO: change this to the appropriate type once it is set up
 		} & DefaultSession['user'];
 	}
 }
