7c5ccdd29246cfa0727e51f5dbed1f885a7aae46
Daniel J. Antonio <djedidiaha@gmail.com>
2022-11-15 00:40:44 +0700

    custom router template to handle layout in the future

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,15 +6,33 @@ import { trpc } from '../lib/trpc';
 
 import '../styles/globals.css';
 import { AuthProvider } from '../lib/hooks/useAuth';
+import { useRouter } from 'next/router';
+import { FC, PropsWithChildren } from 'react';
 
 const MyApp: AppType<{ session: Session | null }> = ({ Component, pageProps: { session, ...pageProps } }) => {
 	return (
 		<SessionProvider session={session}>
 			<AuthProvider>
-				<Component {...pageProps} />
+				<CustomRouter>
+					<Component {...pageProps} />
+				</CustomRouter>
 			</AuthProvider>
 		</SessionProvider>
 	);
+};
+
+const CustomRouter: FC<PropsWithChildren> = ({ children }) => {
+	const router = useRouter();
+	const basePath = router.pathname.split('/')[1];
+
+	switch (basePath) {
+		case 'admin':
+			return <div id="admin-layout">{children}</div>;
+		case 'teacher':
+			return <div id="teacher-layout">{children}</div>;
+		default:
+			return <div>{children}</div>;
+	}
 };
 
 export default trpc.withTRPC(MyApp);
