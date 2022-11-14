4c489a7be87a0de9507f94d672c1c50658445345
Daniel J. Antonio <djedidiaha@gmail.com>
2022-11-15 00:41:18 +0700

    custom hook to get routes

---
+++ src/lib/hooks/useCustomRoute.tsx
@@ -0,0 +1,24 @@
+import { useRouter } from 'next/router';
+
+export const useCustomRoute = () => {
+	const router = useRouter();
+	const _path = router.pathname.split('/')[1];
+
+	let basePath = '/';
+	switch (_path) {
+		case 'admin':
+			basePath = '/admin';
+			break;
+		case 'teacher':
+			basePath = '/teacher';
+			break;
+		default:
+			break;
+	}
+
+	const getNewRoute = (route: string) => {
+		return `${basePath}/${route}`;
+	};
+
+	return { basePath, getNewRoute };
+};
