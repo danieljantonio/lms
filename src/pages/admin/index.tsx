02efaf585c255f4e31fa13e941f4b5b4fc00d751
Daniel J. Antonio <djedidiaha@gmail.com>
2022-11-16 16:52:42 +0700

    move school admin functions to school tab

--- src/pages/admin/index.tsx
+++ src/pages/admin/index.tsx
@@ -1,7 +1,4 @@
-import { Card } from 'flowbite-react';
 import { type NextPage } from 'next';
-import CreateSchoolCard from '../../components/admin/school/create-card.school';
-import JoinSchoolCard from '../../components/admin/school/join-card.school';
 
 const TeacherPanel: NextPage = () => {
 	return (
@@ -11,13 +8,6 @@ const TeacherPanel: NextPage = () => {
 				This will be a place where all the functions will be created. It'll have all the helper functions that
 				would aid in the development of the whole app.
 			</p>
-			<Card>
-				School Actions
-				<div className="flex gap-4">
-					<CreateSchoolCard />
-					<JoinSchoolCard />
-				</div>
-			</Card>
 		</div>
 	);
 };
--- src/pages/admin/school.tsx
+++ src/pages/admin/school.tsx
@@ -1,5 +1,20 @@
+import { Card } from 'flowbite-react';
+import CreateSchoolCard from '../../components/admin/school/create-card.school';
+import JoinSchoolCard from '../../components/admin/school/join-card.school';
+
 const AdminSchool = () => {
-	return <div>This is the admin's school panel</div>;
+	return (
+		<div className="flex flex-col justify-center gap-2">
+			<p>This is the admin's school panel</p>
+			<Card>
+				School Actions
+				<div className="flex gap-4">
+					<CreateSchoolCard />
+					<JoinSchoolCard />
+				</div>
+			</Card>
+		</div>
+	);
 };
 
 export default AdminSchool;
