9b5785845bf41548c3da22775463145919f6eca1
Daniel J. Antonio <djedidiaha@gmail.com>
2022-11-29 14:13:55 +0700

    list all available schools

--- src/pages/admin/school.tsx
+++ src/pages/admin/school.tsx
@@ -1,18 +1,37 @@
 import { Card } from 'flowbite-react';
 import CreateSchoolCard from '../../components/admin/school/create-card.school';
 import JoinSchoolCard from '../../components/admin/school/join-card.school';
+import { trpc } from '../../lib/trpc';
 
 const AdminSchool = () => {
+	const { data: schools, isLoading: schoolsIsLoading } = trpc.school.getAll.useQuery();
+
 	return (
-		<div className="flex flex-col justify-center gap-2">
+		<div className="flex flex-col items-start gap-2">
 			<p>This is the admin's school panel</p>
-			<Card>
-				School Actions
-				<div className="flex gap-4">
-					<CreateSchoolCard />
-					<JoinSchoolCard />
-				</div>
-			</Card>
+			<div className="flex w-full justify-between">
+				<Card>
+					School Actions
+					<div className="flex gap-4">
+						<CreateSchoolCard />
+						<JoinSchoolCard />
+					</div>
+				</Card>
+				<Card>
+					{schoolsIsLoading ? (
+						<div>Loading...</div>
+					) : (
+						<div>
+							<p className="text-lg font-bold">List of Available Schools</p>
+							{schools?.map((school, i) => (
+								<p>
+									{school.name} - {school.code}
+								</p>
+							))}
+						</div>
+					)}
+				</Card>
+			</div>
 		</div>
 	);
 };
