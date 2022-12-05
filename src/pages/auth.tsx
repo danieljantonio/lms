2e0ffa544e0483d34a3ca595012a032853f05024
Daniel J. Antonio <djedidiaha@gmail.com>
2022-12-06 01:58:08 +0700

    add auth page template

---
+++ src/pages/auth.tsx
@@ -0,0 +1,24 @@
+import { Button } from 'flowbite-react';
+import { NextPage } from 'next';
+import CommonLayout from '../components/layouts/common.layout';
+
+const AuthPage: NextPage = () => {
+	return (
+		<CommonLayout>
+			<div className="flex h-full items-center justify-center">
+				<div className="w-5/12">
+					<div className="flex flex-col items-start justify-center gap-6 px-14">
+						<p className="text-8xl">Ignosi</p>
+						<p className="font-serif text-lg">Start your education journey with us.</p>
+					</div>
+				</div>
+				<div className="flex w-7/12 flex-col items-center justify-center gap-10">
+					<p className="text-4xl">Welcome to Ignosi!</p>
+					<Button className="px-10">Login with Google</Button>
+				</div>
+			</div>
+		</CommonLayout>
+	);
+};
+
+export default AuthPage;
