4faea2fdd878936cdd63b626fd5c0e4e026c9ae6
Daniel Antonio <djedidiaha@gmail.com>
2022-12-12 04:09:55 +0700

    add app pages boilerplate

---
+++ src/pages/app/students.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Students: NextPage = () => {
+	return <div>Students Page, contain the list of students along with some stats</div>;
+};
+
+export default Students;
---
+++ src/pages/app/tests.tsx
@@ -0,0 +1,7 @@
+import { NextPage } from 'next';
+
+const Tests: NextPage = () => {
+	return <div>Test Page, contain the list of existing tests, create test button, and some analytics</div>;
+};
+
+export default Tests;
