0d8f8d277b17c636c9a646ad817e40b28792e1d8
Daniel J. Antonio <djedidiaha@gmail.com>
2023-01-05 16:54:00 +0700

    remove unused pages (old admin and teacher pages)

--- src/pages/admin/students.tsx
+++
@@ -1,5 +0,0 @@
-const AdminSchool = () => {
-	return <div>This is the admin's students panel</div>;
-};
-
-export default AdminSchool;
--- src/pages/admin/tests.tsx
+++
@@ -1,5 +0,0 @@
-const AdminSchool = () => {
-	return <div>This is the admin's tests panel</div>;
-};
-
-export default AdminSchool;
