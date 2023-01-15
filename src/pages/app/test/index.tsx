348650f9eedbd1d913e5533a131884baf1a38830
Daniel J. Antonio <djedidiaha@gmail.com>
2023-01-15 14:33:02 +0700

    change create test button

--- src/pages/app/test/index.tsx
+++ src/pages/app/test/index.tsx
@@ -1,12 +1,19 @@
 import { Card } from 'flowbite-react';
 import { NextPage } from 'next';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 const Tests: NextPage = () => {
+	const router = useRouter();
 	return (
-		<div>
-			<p className="text-2xl">Create New Test</p>
-			<Link href="/app/test/create">Create Test</Link>
+		<div className="flex flex-col">
+			{/* <p className="text-2xl">Create New Test</p> */}
+			<Card
+				className="w-48 items-center hover:cursor-pointer hover:bg-gray-100"
+				onClick={() => router.push('/app/test/create')}>
+				Create New Test
+			</Card>
+			{/* <Link href="/app/test/create">Create Test</Link> */}
 		</div>
 	);
 };
