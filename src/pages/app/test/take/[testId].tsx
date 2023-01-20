f3261668a44e2d23a57b47fd525c0fa05b98a454
Daniel J. Antonio <djedidiaha@gmail.com>
2023-01-20 11:20:09 +0700

    add template pages to view, edit, and take tests

---
+++ src/pages/app/test/take/[testId].tsx
@@ -0,0 +1,40 @@
+import { Button } from 'flowbite-react';
+import { NextPage } from 'next';
+import { useRouter } from 'next/router';
+import { validateTestIsOngoing } from '../../../../lib/helpers/date.helpers';
+import { trpc } from '../../../../lib/trpc';
+
+type ClassroomQueryProp = {
+	testId: string;
+};
+
+const TakeTest: NextPage = () => {
+	const router = useRouter();
+	const { testId } = router.query as ClassroomQueryProp;
+
+	const { data, isLoading } = trpc.test.getTestById.useQuery({ testId });
+
+	if (isLoading) return <div>Loading...</div>;
+	if (!data) return <div>Test not found</div>;
+
+	const testIsValid = validateTestIsOngoing(data.startDate, data.endDate);
+
+	if (!testIsValid) router.push('/app/test');
+
+	return testIsValid ? (
+		<div>
+			<div className="mb-6 text-2xl">{data?.name}</div>
+			<div>
+				<p>Duration: {data.duration}</p>
+				<p>Start Date: {data.startDate.toLocaleDateString('en-GB')}</p>
+				<p>End Date: {data.endDate.toLocaleDateString('en-GB')}</p>
+				<p>Questions: {data.questions.length}</p>
+				<Button disabled={!testIsValid} onClick={() => router.push(`/app/test/take/${testId}`)}>
+					Take Test
+				</Button>
+			</div>
+		</div>
+	) : null;
+};
+
+export default TakeTest;
