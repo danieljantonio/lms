de6d4a4e2d8c77cd251f34b7619a6923912e8046
Daniel J. Antonio <djedidiaha@gmail.com>
2023-01-27 14:01:22 +0700

    add take test mutation

--- src/server/trpc/router/tests.ts
+++ src/server/trpc/router/tests.ts
@@ -1,6 +1,7 @@
 import { Test, MCQQuestion, Classroom } from '@prisma/client';
 import { TRPCError } from '@trpc/server';
 import { z } from 'zod';
+import { shuffleArray } from '../../../lib/helpers/common.helpers';
 import { protectedProcedure, router } from '../trpc';
 
 const MCQChoices = z.object({
@@ -133,6 +134,59 @@ export const testRouter = router({
 				},
 			});
 			if (!test) throw new TRPCError({ code: 'NOT_FOUND', message: 'Test not found' });
+			const questionList = shuffleArray(test.questions);
+			console.log(questionList);
+
 			return test;
+		}),
+	take: protectedProcedure
+		.input(
+			z.object({
+				testId: z.string(),
+				classroomId: z.string(),
+			}),
+		)
+		.mutation(async ({ input, ctx }) => {
+			if (ctx.session.user.role !== 'STUDENT')
+				throw new TRPCError({ code: 'FORBIDDEN', message: 'Only students are allowed to take the test' });
+
+			const classroom = (await ctx.prisma.classroom.findUnique({
+				where: { id: input.classroomId },
+			})) as Classroom;
+
+			if (!classroom)
+				throw new TRPCError({ code: 'NOT_FOUND', message: 'You are not allowed to perform the function.' });
+
+			console.log(`${classroom ? 'Classroom exists' : 'Classroom not found'}`);
+
+			const test = await ctx.prisma.test.findUnique({
+				where: { id: input.testId },
+				include: {
+					questions: true,
+				},
+			});
+			if (!test) throw new TRPCError({ code: 'NOT_FOUND', message: 'Test not found' });
+
+			const newStudentTest = await ctx.prisma.studentTest.create({
+				data: {
+					userId: ctx.session.user.id,
+					testId: test.id,
+					startDate: new Date(),
+				},
+			});
+
+			const questionList = shuffleArray(test.questions) as MCQQuestion[];
+
+			questionList.forEach(async (question, index) => {
+				await ctx.prisma.questionsOnStudentTest.create({
+					data: {
+						questionNo: index + 1,
+						questionId: question.id,
+						studentTestId: newStudentTest.id,
+					},
+				});
+			});
+
+			return newStudentTest;
 		}),
 });
