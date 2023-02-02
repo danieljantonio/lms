6b9b4f33cb6b2bcb89c20fdc1960639da70d823e
Daniel J. Antonio <djedidiaha@gmail.com>
2023-02-02 13:56:33 +0700

    question detail component

---
+++ src/components/tests/question-details.tests.tsx
@@ -0,0 +1,32 @@
+import { FC, useState } from 'react';
+import { trpc } from '../../lib/trpc';
+import ChoiceCard from './choice-card.tests';
+
+type QuestionDetailsProps = {
+	id: string;
+	selectedAnswer: (e: string) => void;
+};
+
+const QuestionDetails: FC<QuestionDetailsProps> = ({ id }) => {
+	const { data: question, isLoading: loadingQuestion } = trpc.question.getById.useQuery({ id });
+	const [selectedId, setSelected] = useState<string>();
+
+	const onSelectChoice = (e: string) => {
+		setSelected(e);
+	};
+
+	return loadingQuestion ? (
+		<div>Loading...</div>
+	) : (
+		<div>
+			<p className="text-2xl">Question: {question?.question}</p>
+			{question?.choices.map((choice, index) => (
+				<ChoiceCard key={index} value={choice.id} selected={choice.id === selectedId} onClick={onSelectChoice}>
+					{choice.answer}
+				</ChoiceCard>
+			))}
+		</div>
+	);
+};
+
+export default QuestionDetails;
