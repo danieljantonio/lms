2363858e5397d32e2c94fd5dd22076f916b02b94
Daniel J. Antonio <djedidiaha@gmail.com>
2023-03-31 17:08:33 +0700

    code cleanup (unused components and code)

--- src/pages/app/test/index.tsx
+++ src/pages/app/test/index.tsx
@@ -1,37 +1,12 @@
 import { NextPage } from 'next';
-import { useRouter } from 'next/router';
-import TestCard from '../../../components/tests/common/test-card.tests';
-import useAuth from '../../../lib/hooks/useAuth';
-import { trpc } from '../../../lib/trpc';
 
 const Tests: NextPage = () => {
-	const router = useRouter();
-	const { data, isLoading } = trpc.test.getDashboardData.useQuery(undefined, { refetchOnWindowFocus: false });
-	const { role } = useAuth();
-
-	if (isLoading) return <div>Loading...</div>;
-	if (!data) return <div>No Test Found</div>;
-
 	return (
 		<div className="flex flex-col">
-			<div>Change this to display a table of tests containing proper information</div>
-			{/* <div className="text-xl">Tests {data.tests && `(${data.tests.length})`}</div>
-			{data.tests.length > 0 ? (
-				data.tests.map((test) => <TestCard key={test.id} test={test} />)
-			) : (
-				<div className="mt-4 w-full rounded-md border p-10 text-center">No Upcoming Tests</div>
-			)}
-
-			<div className="mt-4 text-xl">Completed {data.doneTests && `(${data.doneTests.length})`}</div>
-			{data.doneTests.length > 0 ? (
-				data.doneTests.map(
-					(test) =>
-						test.test &&
-						test.score && <TestCard overdue key={test.id} grade={test.score} test={test.test} />,
-				)
-			) : (
-				<div className="mt-4 w-full rounded-md border p-10 text-center">No Ongoing Tests</div>
-			)} */}
+			<div>
+				This will be a place for the teachers to manage all of the tests
+				they have.
+			</div>
 		</div>
 	);
 };
