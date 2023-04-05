0abba366f7c4a16911c79e787c615d70bdab05fc
Daniel J. Antonio <djedidiaha@gmail.com>
2023-04-06 00:27:30 +0700

    new test ui + new prompt

--- src/components/tests/common/start-prompt.tests.tsx
+++ src/components/tests/common/start-prompt.tests.tsx
@@ -1,50 +1,55 @@
-import { Test } from '@prisma/client';
+import { type TestTemplate } from '@prisma/client';
 import { Button, Modal } from 'flowbite-react';
 import { FC, PropsWithChildren } from 'react';
 
 type Props = {
-	toggle: (open: boolean) => void;
-	isOpen: boolean;
-	onStartTest: () => void;
-	testDetails: Test;
+	onTakeTest: () => void;
+	testDetails: TestTemplate;
+	loading: boolean;
 };
 
 const StartPrompt: FC<PropsWithChildren<Props>> = ({
-	toggle,
-	isOpen,
-	onStartTest,
+	onTakeTest,
 	testDetails,
+	loading,
 }) => {
-	const closeModal = () => toggle(false);
-
 	return (
-		<Modal show={isOpen} onClose={closeModal}>
-			<Modal.Header>
-				Do you want to start the test - {testDetails.name}?
-			</Modal.Header>
-			<Modal.Body>
-				<ul className="list-disc p-6">
-					<li>
-						You have {testDetails.duration} minutes from the moment
-						you start the test.
-					</li>
-					<li>You may not stop in the middle of the test.</li>
-					<li>
-						If you fail to complete all the questions within the
-						given time, you will only be marked on what you have
-						finished.
-					</li>
-				</ul>
-			</Modal.Body>
-			<Modal.Footer>
-				<Button disabled={false} onClick={onStartTest}>
-					Start Test
-				</Button>
-				<Button disabled={false} color="gray" onClick={closeModal}>
-					Cancel
-				</Button>
-			</Modal.Footer>
-		</Modal>
+		<>
+			<input
+				type="checkbox"
+				id="test-start-prompt"
+				className="modal-toggle"
+			/>
+			<div className="modal">
+				<div className="modal-box relative">
+					<label
+						htmlFor="test-start-prompt"
+						className="btn btn-sm btn-circle absolute right-2 top-2">
+						✕
+					</label>
+					<ul className="list-disc p-6">
+						<li>
+							You have {testDetails.duration} minutes from the
+							moment you start the test.
+						</li>
+						<li>You may not stop in the middle of the test.</li>
+						<li>
+							If you fail to complete all the questions within the
+							given time, you will only be marked on what you have
+							finished.
+						</li>
+					</ul>
+					<div className="modal-action">
+						<button
+							disabled={loading}
+							className="btn"
+							onClick={onTakeTest}>
+							Take Test
+						</button>
+					</div>
+				</div>
+			</div>
+		</>
 	);
 };
 
