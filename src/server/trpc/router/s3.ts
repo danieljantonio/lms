58785ed0b439dcc0fd804b247cc1b0c271846bb4
Daniel J. Antonio <djedidiaha@gmail.com>
2023-04-11 23:54:06 +0700

    setup aws trpc routes

---
+++ src/server/trpc/router/s3.ts
@@ -0,0 +1,34 @@
+import { z } from 'zod';
+import { router, protectedProcedure } from '../trpc';
+import { env } from '@/lib/env/server.mjs';
+import { PutObjectCommand } from '@aws-sdk/client-s3';
+import { getSignedUrl } from '@aws-sdk/s3-request-presigner';
+
+export const s3Router = router({
+	getObjects: protectedProcedure.query(async ({ ctx }) => {
+		const { s3 } = ctx;
+
+		const listObjectsOutput = await s3.listObjectsV2({
+			Bucket: env.AWS_BUCKET_NAME,
+		});
+
+		return listObjectsOutput.Contents ?? [];
+	}),
+	getStandardUploadPresignedUrl: protectedProcedure
+		.input(
+			z.object({
+				key: z.string(),
+			}),
+		)
+		.mutation(async ({ ctx, input }) => {
+			const { key } = input;
+			const { s3 } = ctx;
+
+			const putObjectCommand = new PutObjectCommand({
+				Bucket: env.AWS_BUCKET_NAME,
+				Key: key,
+			});
+
+			return await getSignedUrl(s3, putObjectCommand);
+		}),
+});
