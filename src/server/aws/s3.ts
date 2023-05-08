19a223d33366e3529b5ef858461cbc83b6fa21de
Daniel J. Antonio <djedidiaha@gmail.com>
2023-05-08 23:04:39 +0700

    fix vercel build env issues

--- src/server/aws/s3.ts
+++ src/server/aws/s3.ts
@@ -4,7 +4,7 @@ import { env } from '@/lib/env/server.mjs';
 export const s3 = new S3({
 	region: env.AWS_REGION,
 	credentials: {
-		accessKeyId: env.AWS_ACCESS_KEY,
+		accessKeyId: env.AWS_ACCESS_KEY_ID,
 		secretAccessKey: env.AWS_SECRET_ACCESS_KEY,
 	},
 });
