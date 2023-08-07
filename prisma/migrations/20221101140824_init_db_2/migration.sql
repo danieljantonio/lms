f04fd03cd68a54369bb794618c3deebf0a125257
Daniel Antonio <jedidiah.main@gmail.com>
2023-08-07 22:23:43 +0700

    remove migrations

--- prisma/migrations/20221101140824_init_db_2/migration.sql
+++
@@ -1,9 +0,0 @@
-/*
-  Warnings:
-
-  - The `wrongAnswers` column on the `MCQQuestion` table would be dropped and recreated. This will lead to data loss if there is data in the column.
-
-*/
--- AlterTable
-ALTER TABLE "MCQQuestion" DROP COLUMN "wrongAnswers",
-ADD COLUMN     "wrongAnswers" TEXT[];
